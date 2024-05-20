b84b87a051e776b131bf47d9be8eec1c52f4e458
Annazubakha <145809450+Annazubakha@users.noreply.github.com>
2024-05-20 14:52:38 +0000

    Deploying to gh-pages from @ Annazubakha/nanny-services@80acfe836489f2f51e4384c2e9c3d7fe2399cc1c 🚀

--- assets/Favorites-bcd451b5.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-7cb3f235.js";import{F as s}from"./Filters-fe2328d8.js";const e="_wrapper_1w2zx_1",p={wrapper:e},o=()=>r.jsx("div",{className:p.wrapper,children:r.jsx(s,{})});export{o as default};
